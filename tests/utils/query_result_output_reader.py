5bc53441f984eefc3bbe73746db9cef7a43eefd6
Jesabel Pugliese <jesabelpugliese004@gmail.com>
2026-05-25 17:29:27 -0300

    fix(query_result_output_reader): update _parse_q2_output to include from_bank_name

--- tests/utils/query_result_output_reader.py
+++ tests/utils/query_result_output_reader.py
@@ -25,10 +25,10 @@ class QueryResultOutputReader:
         [from_bank_id, from_account, to_bank_id, to_account, amount] = row
         return [int(from_bank_id), from_account, int(to_bank_id), to_account, float(amount)]
     
-    # TODO: Change for bank name
+    # TODO: Row should be [from_bank_name, account, from_bank_id, amount]
     def _parse_q2_output(self, row):
-        [from_bank_id, account, amount] = row
-        return [int(from_bank_id), account, float(amount)]
+        [from_bank_name, account, amount] = row
+        return [from_bank_name, account, float(amount)]
 
     def _parse_q3_output(self, row):
         [from_bank_id, from_account, amount, payment_format] = row
